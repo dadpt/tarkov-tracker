9bc16fae55e7bba3b9483b3065aedd0d26df011d
Michael Oviedo <michaeloviedo2000@gmail.com>
2023-01-02 20:20:33 -0500

    backend set up w/express and typescript

---
+++ backend/dist/server.js
@@ -0,0 +1,8 @@
+"use strict";
+const express = require('express');
+// express app
+const app = express();
+// listen for requests
+app.listen(4000, () => {
+    console.log('listening on port 4000!!!');
+});
